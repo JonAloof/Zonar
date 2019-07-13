129610dc6a24fa636e52e64f869bbbcbd29dfebc
Jonathan Aloof <jonathan@unboxthepattern.com>
2019-07-13 15:02:36 -0700

    Initial commit with helloworld api

---
+++ README.md
@@ -0,0 +1,11 @@
+# Zonar
+
+Setup Instructions
+
+Create virtual environment:
+
+py -3 -m venv env
+
+Install Dependencies:
+
+pip install -r requirements.txt
---
+++ helloworld.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_restful import Api
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route('/')
+def index():
+    return "Hello World", 200
+
+app.run(debug=True)
