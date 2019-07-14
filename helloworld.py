00033b3cd74504733e33c1c400ccd8860ee0cdc8
JonAloof <jonathan@unboxthepattern.com>
2019-07-14 16:36:07 -0700

    Added sentry configuration

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,11 @@
+import sentry_sdk
 from flask import Flask
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_sdk.init(
+    dsn="http://5f20a399172749bea73d2d0ed7556fe5@zonar.unboxthepattern.com:9000/2",
+    integrations=[FlaskIntegration()]
+)
 
 app = Flask(__name__)
 
