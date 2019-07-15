796d8b2900462b5e4c9a3c1b204bc75343055a20
JonAloof <jonathan@unboxthepattern.com>
2019-07-14 17:22:04 -0700

    Added build number api call into jenkins pipeline

--- helloworld.py
+++ helloworld.py
@@ -3,9 +3,11 @@ from flask import Flask
 from sentry_sdk.integrations.flask import FlaskIntegration
 
 sentry_sdk.init(
-    dsn="http://5f20a399172749bea73d2d0ed7556fe5@zonar.unboxthepattern.com:9000/2",
+    dsn="http://203b6ccf1913474ea7a5d2b15c6b58f7@zonar.unboxthepattern.com:9000/3",
     integrations=[FlaskIntegration()]
 )
+
+sentry_sdk.capture_message("Test")
 
 app = Flask(__name__)
 
